a5d80f6c123adfa5c996bcb17f07584e41816363
DarshanasZone <cdarshana219@gmail.com>
2026-02-07 19:56:34 +0530

    Day 24 - Java Varargs (Simple Addition)

---
+++ Day24/Java_Varargs.java
@@ -0,0 +1,91 @@
+
+/*
+HackerRank Java Day 24
+Problem: Java Varargs - Simple Addition
+
+You are given a class Solution and its main method.
+Your task is to create the class Add and the required methods so that the code prints
+the sum of the numbers passed to the function add.
+
+Note:
+Your add method must use varargs and method overloading is NOT allowed.
+
+Sample Output:
+1+2=3
+1+2+3=6
+1+2+3+4+5=15
+1+2+3+4+5+6=21
+*/
+
+
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Add {
+    void add(int... numbers) {
+        int sum = 0;
+        
+        for(int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+            System.out.print(numbers[i]);
+            
+            if(i != numbers.length - 1) {
+                System.out.print("+");
+            }
+        }
+        
+        System.out.println("=" + sum);
+    }
+}
+
+
+
+public class Java_Varargs {
+
+    public static void main(String[] args) {
+       try{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			int n1=Integer.parseInt(br.readLine());
+			int n2=Integer.parseInt(br.readLine());
+			int n3=Integer.parseInt(br.readLine());
+			int n4=Integer.parseInt(br.readLine());
+			int n5=Integer.parseInt(br.readLine());
+			int n6=Integer.parseInt(br.readLine());
+			Add ob=new Add();
+			ob.add(n1,n2);
+			ob.add(n1,n2,n3);
+			ob.add(n1,n2,n3,n4,n5);	
+			ob.add(n1,n2,n3,n4,n5,n6);
+			Method[] methods=Add.class.getDeclaredMethods();
+			Set<String> set=new HashSet<>();
+			boolean overload=false;
+			for(int i=0;i<methods.length;i++)
+			{
+				if(set.contains(methods[i].getName()))
+				{
+					overload=true;
+					break;
+				}
+				set.add(methods[i].getName());
+				
+			}
+			if(overload)
+			{
+				throw new Exception("Overloading not allowed");
+			}
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+	
+	
+
+}
+
