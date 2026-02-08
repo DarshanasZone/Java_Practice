7cbc4a6c3def69aaef66dda83c291b424576177d
DarshanasZone <cdarshana219@gmail.com>
2026-02-08 15:33:33 +0530

    Day25 : Java Reflection and Inner Class problems

---
+++ Day25/JavaInnerPrivateClass.java
@@ -0,0 +1,86 @@
+/*
+Day25 - Java Inner Private Class (HackerRank)
+Call private inner class method using reflection
+
+
+Sample Input
+8
+
+
+Sample Output
+8 is power of 2
+An instance of class: Solution.Inner.Private has been created
+*/
+
+
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.*;
+import java.util.regex.*;
+import java.security.*;
+
+
+public class JavaInnerPrivateClass {
+
+	public static void main(String[] args) throws Exception {
+		DoNotTerminate.forbidExit();	
+
+		try{
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			int num = Integer.parseInt(br.readLine().trim());
+			Object o;// Must be used to hold the reference of the instance of the class Solution.Inner.Private
+// Create Inner class object
+            JavaInnerPrivateClass.Inner inner = new JavaInnerPrivateClass.Inner();
+
+            // Access private inner class via reflection
+            Class<?> privateClass = Class.forName("Solution$Inner$Private");
+            Constructor<?> constructor = privateClass.getDeclaredConstructor(JavaInnerPrivateClass.Inner.class);
+            constructor.setAccessible(true);
+
+            // Create object of private class
+            o = constructor.newInstance(inner);
+
+            // Access private method powerof2
+            Method method = privateClass.getDeclaredMethod("powerof2", int.class);
+            method.setAccessible(true);
+
+            String result = (String) method.invoke(o, num);
+            System.out.println(num + " is " + result);
+		System.out.println("An instance of class: " + o.getClass().getCanonicalName() + " has been created");
+		
+		}//end of try
+		
+		catch (DoNotTerminate.ExitTrappedException e) {
+			System.out.println("Unsuccessful Termination!!");
+		}
+	}//end of main
+	static class Inner{
+		private class Private{
+			private String powerof2(int num){
+				return ((num&num-1)==0)?"power of 2":"not a power of 2";
+			}
+		}
+	}//end of Inner
+	
+}
+
+class DoNotTerminate { //This class prevents exit(0)
+	 
+    public static class ExitTrappedException extends SecurityException {
+
+		private static final long serialVersionUID = 1L;
+    }
+ 
+    public static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
+}	
+	
